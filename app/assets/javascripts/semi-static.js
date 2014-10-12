5c28cbda228c10819ce9827c39f4c918c81a6c79
Brod Justice <brod@businness-landing.com>
2014-10-13 00:42:01 +0200

    CSS reworking to isolate, version bump to 0.0.2

---
+++ app/assets/javascripts/semi-static.js
@@ -0,0 +1,12 @@
+function semiStaticOpenMenu(){
+  document.getElementById('menu').style.display='block';
+  document.getElementById('menu-close').style.display='block';
+  document.getElementById('bar-in-menu').style.display='block';
+  document.getElementById('menu-button').style.display='none';
+}
+function semiStaticCloseMenu(){
+  document.getElementById('menu').style.display='none';
+  document.getElementById('menu-close').style.display='none';
+  document.getElementById('bar-in-menu').style.display='none';
+  document.getElementById('menu-button').style.display='block';
+}
--- app/assets/javascripts/semi_static_application.js
+++ app/assets/javascripts/semi_static_application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require semi-static
 //= require_tree .
