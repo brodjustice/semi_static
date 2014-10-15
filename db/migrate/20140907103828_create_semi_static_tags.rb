75ee0fe10808f80a3c506913da8ec8f3960c1350
Brod Justice <brod@businness-landing.com>
2014-10-15 12:06:43 +0200

    Various layout improvements

--- db/migrate/20140907103828_create_semi_static_tags.rb
+++ db/migrate/20140907103828_create_semi_static_tags.rb
@@ -4,14 +4,14 @@ class CreateSemiStaticTags < ActiveRecord::Migration
       t.string :name
       t.string :slug
       t.boolean :menu, :default => false
-      t.integer :position, :default => 0
+      t.integer :position, :default => 1
       t.string :predefined_class, :default => nil
       t.string :locale, :default => 'en'
       t.integer :max_entries_on_index_page, :default => 3
       t.string :sidebar_title
 
       # CSS
-      t.string :colour, :default => 'black'
+      t.string :colour, :default => 'inherit'
 
       # Icon as attachment
       t.string :icon_file_name
