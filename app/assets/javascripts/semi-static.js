c3d10e216bbd33070c9acab7e8391ad9db9b8a9f
Brod Justice <brod@businness-landing.com>
2014-10-30 12:41:01 +0100

    Added very simple loading script to menu box for mobile

--- app/assets/javascripts/semi-static.js
+++ app/assets/javascripts/semi-static.js
@@ -1,3 +1,16 @@
+function semiStaticLoading(){
+  document.getElementById('menu-main').style.display='none';
+  document.getElementById('menu-close').style.display='none';
+  document.getElementById('bar-in-menu').style.display='none';
+  var el = document.getElementById('menu-loading'),
+    i = 0,
+    load = setInterval(function() {
+      i = ++i % 10;
+      el.innerHTML = '&#9609;' + Array(i + 1).join('&#9609;');
+  }, 400);
+  true
+}
+
 function semiStaticOpenMenu(){
   document.getElementById('menu').style.display='block';
   document.getElementById('menu-close').style.display='block';
