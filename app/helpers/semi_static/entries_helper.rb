b5e806c95f0a5063111b30edc0906b757dc80580
Brod Justice <brod@businness-landing.com>
2014-11-30 11:49:16 +0100

    Stop related videos display at end of embedded YouTube

--- app/helpers/semi_static/entries_helper.rb
+++ app/helpers/semi_static/entries_helper.rb
@@ -3,7 +3,7 @@ module SemiStatic
     STYLE_CLASSES = ['normal', ' feint',  'flat',  'collapse', 'flat collapse', 'hard', 'highlight', 'wobble', 'dotted']
 
     def youtube_video(id)
-      "<div class='yt_video'> <iframe width='640' height='360' src='//www.youtube.com/embed/#{id}?controls=0&amp;showinfo=0' allowfullscreen></iframe></div>".html_safe
+      "<div class='yt_video'> <iframe width='640' height='360' src='//www.youtube.com/embed/#{id}?rel=0&amp;controls=0&amp;showinfo=0' allowfullscreen></iframe></div>".html_safe
     end
   end
 end
