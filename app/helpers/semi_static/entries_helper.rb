46ca54a691a5929b5b7d6263a014a3e97acfdd2b
Brod Justice <brod@businness-landing.com>
2014-12-14 20:42:39 +0100

    Better document download size calculation

--- app/helpers/semi_static/entries_helper.rb
+++ app/helpers/semi_static/entries_helper.rb
@@ -5,5 +5,12 @@ module SemiStatic
     def youtube_video(id)
       "<div class='yt_video'> <iframe width='640' height='360' src='//www.youtube.com/embed/#{id}?rel=0&amp;controls=0&amp;showinfo=0' allowfullscreen></iframe></div>".html_safe
     end
+
+    def human_file_size(s)
+      units = %w{b Kb Mb Gb Tb}
+      e = (Math.log(s)/Math.log(1024)).floor
+      s = "%.0f" % (s.to_f / 1024**e)
+      s.sub(/\.?0*$/, units[e])
+    end
   end
 end
