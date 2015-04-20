6d147aab080305540558ad717d4eb9dabbd9b668
Brod Justice <brod@businness-landing.com>
2015-04-20 14:13:29 +0200

    Add custom classes for entries

--- app/helpers/semi_static/entries_helper.rb
+++ app/helpers/semi_static/entries_helper.rb
@@ -1,6 +1,6 @@
 module SemiStatic
   module EntriesHelper
-    STYLE_CLASSES = ['normal', ' feint',  'flat',  'collapse', 'flat collapse', 'hard', 'highlight', 'wobble', 'dotted', 'tile']
+    STYLE_CLASSES = ['normal', ' feint',  'flat',  'collapse', 'flat collapse', 'hard', 'highlight', 'wobble', 'dotted', 'tile', 'custom1', 'custom2', 'custom3']
 
     def youtube_video(id)
       "<div class='yt_video'> <iframe width='640' height='360' src='//www.youtube.com/embed/#{id}?rel=0&amp;controls=0&amp;showinfo=0' allowfullscreen></iframe></div>".html_safe
