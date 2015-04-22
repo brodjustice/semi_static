5dc9c4f7d16f10ae3b56cc14e29830c0aef1d5d6
Brod Justice <brod@businness-landing.com>
2015-04-22 09:15:48 +0200

    CSS improvements

--- app/assets/javascripts/semi-static.js
+++ app/assets/javascripts/semi-static.js
@@ -3,6 +3,7 @@ function semiStaticOpenMenu(){
   document.getElementById('menu-close').style.display='block';
   document.getElementById('bar-in-menu').style.display='block';
   document.getElementById('menu-button').style.display='none';
+  document.getElementById('bar_menu').style.display='block';
 }
 function semiStaticCloseMenu(){
   document.getElementById('menu-loading').style.display='none';
@@ -10,4 +11,5 @@ function semiStaticCloseMenu(){
   document.getElementById('menu-close').style.display='none';
   document.getElementById('bar-in-menu').style.display='none';
   document.getElementById('menu-button').style.display='block';
+  document.getElementById('bar_menu').style.display='none';
 }
