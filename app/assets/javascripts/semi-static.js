9c0a03e7b78de28fb5101e5d44327b2517b9d847
Brod Justice <brod@businness-landing.com>
2015-04-24 13:20:41 +0200

    First v2 release, only bannerless theme is tested

--- app/assets/javascripts/semi-static.js
+++ app/assets/javascripts/semi-static.js
@@ -1,15 +1,12 @@
 function semiStaticOpenMenu(){
   document.getElementById('menu').style.display='block';
+  document.getElementById('bar-menu').style.display='block';
   document.getElementById('menu-close').style.display='block';
-  document.getElementById('bar-in-menu').style.display='block';
   document.getElementById('menu-button').style.display='none';
-  document.getElementById('bar_menu').style.display='block';
 }
 function semiStaticCloseMenu(){
-  document.getElementById('menu-loading').style.display='none';
   document.getElementById('menu').style.display='none';
+  document.getElementById('bar-menu').style.display='none';
   document.getElementById('menu-close').style.display='none';
-  document.getElementById('bar-in-menu').style.display='none';
   document.getElementById('menu-button').style.display='block';
-  document.getElementById('bar_menu').style.display='none';
 }
