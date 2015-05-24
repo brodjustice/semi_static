ce9d3149360f31a5d0ab35cd7b928b768140b771
Brod Justice <brod@businness-landing.com>
2015-05-24 23:49:46 +0200

    standard-2co-1col CSS fix for mobile with extra menu items

--- app/assets/javascripts/semi-static.js
+++ app/assets/javascripts/semi-static.js
@@ -1,12 +1,14 @@
 function semiStaticOpenMenu(){
   document.getElementById('menu').style.display='block';
   document.getElementById('bar-menu').style.display='block';
+  document.getElementById('menu-sub').style.display='block';
   document.getElementById('menu-close').style.display='block';
   document.getElementById('menu-button').style.display='none';
 }
 function semiStaticCloseMenu(){
   document.getElementById('menu').style.display='none';
   document.getElementById('bar-menu').style.display='none';
+  document.getElementById('menu-sub').style.display='none';
   document.getElementById('menu-close').style.display='none';
   document.getElementById('menu-button').style.display='block';
 }
