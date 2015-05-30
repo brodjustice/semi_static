85237089a4c31f587aa5ded9bee4fea3567ab1d3
Brod Justice <brod@businness-landing.com>
2015-05-30 18:16:22 +0200

    JS bug fix

--- app/assets/javascripts/semi-static.js
+++ app/assets/javascripts/semi-static.js
@@ -1,14 +1,15 @@
+var menuSub = document.getElementById('menu-sub');
 function semiStaticOpenMenu(){
   document.getElementById('menu').style.display='block';
   document.getElementById('bar-menu').style.display='block';
-  document.getElementById('menu-sub').style.display='block';
+  if (menuSub != null) { menuSub.style.display='block'; }
   document.getElementById('menu-close').style.display='block';
   document.getElementById('menu-button').style.display='none';
 }
 function semiStaticCloseMenu(){
   document.getElementById('menu').style.display='none';
   document.getElementById('bar-menu').style.display='none';
-  document.getElementById('menu-sub').style.display='none';
+  if (menuSub != null) { menuSub.style.display='none'; }
   document.getElementById('menu-close').style.display='none';
   document.getElementById('menu-button').style.display='block';
 }
