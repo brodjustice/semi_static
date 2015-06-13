74921ab6414fda8fe2dd83d3b38f21b120c95118
Brod Justice <brod@businness-landing.com>
2015-06-13 15:32:39 +0200

    Added another rails layout

--- lib/semi_static/general.rb
+++ lib/semi_static/general.rb
@@ -3,7 +3,8 @@ module General
     0 => 'application',
     1 => 'home',
     2 => 'full', 
-    3 => 'embedded_fonts_full'
+    3 => 'embedded_full',
+    4 => 'embedded_fonts_full'
   }
 
   def layout_select(obj)
