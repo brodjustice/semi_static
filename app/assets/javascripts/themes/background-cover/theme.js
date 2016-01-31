9f13c3963c7ff7636f54b3ea625d500cb2830b9c
Brod Justice <brod@businness-landing.com>
2016-02-01 00:49:34 +0100

    Some background cover pre-alpha code

---
+++ app/assets/javascripts/themes/background-cover/theme.js
@@ -0,0 +1,8 @@
+function semiStaticSlideOpenMenu(){
+  var menuWrapper = document.getElementById('menu-wrapper');
+  menuWrapper.style.right='0px';
+}
+function semiStaticSlideCloseMenu(){
+  var menuWrapper = document.getElementById('menu-wrapper');
+  menuWrapper.style.right='-400px';
+}
