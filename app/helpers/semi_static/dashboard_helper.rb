2c31dd32ff0dcacec4239f0f82801f4ea025aed2
brodjustice <brod@business-landing.com>
2019-03-11 19:45:13 +0100

    Closes #39 #38

--- app/helpers/semi_static/dashboard_helper.rb
+++ app/helpers/semi_static/dashboard_helper.rb
@@ -42,8 +42,18 @@ module SemiStatic
     def labeled_checkbox(f, box, text=nil, infomarker=false)
       text ||= box.to_s.humanize
       klass = f.object.class.name.split('::').last.downcase
+      checked = f.object.send(box) ? "checked='checked'" : ''
       infomarker_html = infomarker ? "<span class='infomarker' data-marker='#{infomarker.to_s}'></span>" : ''
-      "<div class='input-group'>#{infomarker_html}<div class='input-group-prepend'> <label class='input-group-text' for='#{['semi_static', klass, box.to_s].join('_')}'>#{text}</label> </div> <div class='form-control mt-2'> <div class='checkbox-wrapper'> <input name='#{klass}[#{box.to_s}]' type='hidden' value='0'><input type='checkbox' value='1' name='#{klass}[#{box.to_s}]' id='#{['semi_static', klass, box.to_s].join('_')}'> </div> </div> </div>".html_safe
+      "<div class='input-group'>#{infomarker_html}<div class='input-group-prepend'> <label class='input-group-text' for='#{['semi_static', klass, box.to_s].join('_')}'>#{text}</label> </div> <div class='form-control mt-2'> <div class='checkbox-wrapper'> <input name='#{klass}[#{box.to_s}]' type='hidden' value='0'><input type='checkbox' value='1' #{checked} name='#{klass}[#{box.to_s}]' id='#{['semi_static', klass, box.to_s].join('_')}'> </div> </div> </div>".html_safe
+    end
+
+    def labeled_textfield(f, attr, text=nil, infomarker=false)
+      text ||= attr.to_s.humanize
+      klass = f.object.class.name.split('::').last.downcase
+      infomarker_html = infomarker ? "<span class='infomarker' data-marker='#{infomarker.to_s}'></span>" : ''
+      input_html = "<input class='form-control mt-2' type='text' value='#{f.object.send(attr)}' name='#{klass}[#{attr}]' id='#{klass}_#{attr}'>".html_safe
+
+      "<div class='input-group'>#{infomarker_html}<div class='input-group-prepend'> <label class='input-group-text' for='#{klass}_#{attr}'>#{text}</label></div>#{input_html}</div>".html_safe
     end
 
     def labeled_selectbox(f, attr, select_options, text=nil, infomarker=false)
