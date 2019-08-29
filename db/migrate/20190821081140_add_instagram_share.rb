6e48f3c028490c9536d500929883b14d1197f5b6
brodjustice <brod@business-landing.com>
2019-08-29 17:48:09 +0200

    Fix migration bug

--- db/migrate/20190821081140_add_instagram_share.rb
+++ db/migrate/20190821081140_add_instagram_share.rb
@@ -1,5 +1,5 @@
 class AddInstagramShare < ActiveRecord::Migration[5.2]
   def change
-    add_column :semi_static_entries, :instagram_share, :boolean, :default => :false
+    add_column :semi_static_entries, :instagram_share, :boolean, :default => false
   end
 end
