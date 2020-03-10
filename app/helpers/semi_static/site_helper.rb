b22b4ab3b7026e05a06be4c3f7566bbdc26dffb1
brodjustice <brod@business-landing.com>
2020-03-10 17:06:36 +0100

    Small improvements

--- app/helpers/semi_static/site_helper.rb
+++ app/helpers/semi_static/site_helper.rb
@@ -541,7 +541,7 @@ module SemiStatic
             page.nil? && (link = u)
           end
           if flags
-            c+= "<li class='locale'><a href='#{link || page.hreflang_link(l) || u}'>#{image_tag("flags/" + l + ".png")}</a></li>".html_safe
+            c+= "<li class='locale'><a href='#{link || page.hreflang_link(l) || u}'>#{image_tag("flags/" + l + ".png", :alt => "locale #{l}")}</a></li>".html_safe
           else
             if session[:locale] == l
               c+= "<li class='locale selected'><a href='#{link || page.hreflang_link(l) || u}'>#{l}</a></li>".html_safe
