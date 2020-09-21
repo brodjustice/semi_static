d835a875f683f7ab218685e4ecdd06a1201f2019
brodjustice <brod@business-landing.com>
2020-09-21 15:25:33 +0200

    Fixed migration for previous commit

--- db/migrate/20200919142157_semi_static_tag_hreflang_tags.rb
+++ db/migrate/20200919142157_semi_static_tag_hreflang_tags.rb
@@ -1,8 +1,9 @@
 class SemiStaticTagHreflangTags < ActiveRecord::Migration[5.2]
   def change
     create_table :semi_static_tag_hreflang_tags, :id => false do |t|
-      t.integer  :setting_tag_id
+      t.integer  :tag_id
       t.integer  :href_tag_id
     end
+    add_column :semi_static_hreflangs, :href_equiv_entry_id, :integer
   end
 end
