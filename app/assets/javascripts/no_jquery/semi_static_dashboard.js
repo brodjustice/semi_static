bd4ee05dea20245f17018fd2f49148399a4d8747
brodjustice <brod@business-landing.com>
2022-09-13 12:47:42 +0200

    Non jquery improvements

--- app/assets/javascripts/no_jquery/semi_static_dashboard.js
+++ app/assets/javascripts/no_jquery/semi_static_dashboard.js
@@ -1,4 +1,3 @@
-// Minimize left menu
 function init___dashboard(){
   document.querySelector('.app-sidebar__toggle').addEventListener('click', e => {
     e.preventDefault();
@@ -9,6 +8,28 @@ function init___dashboard(){
         el.classList.add('sidenav-toggled')
       }
     });
+  });
+
+  document.querySelectorAll("[data-toggle='dropdown']").forEach((el, i) => {
+    el.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelectorAll('.dropdown-menu').forEach((el, i) => {
+        if(el.classList.contains('show')){
+          el.classList.remove('show');
+        } else {
+          el.classList.add('show');
+        }
+      });
+    });
+  });
+
+  // Set event handler for info-help
+  document.getElementById('help-info-button').addEventListener('click', function(){
+    if(document.body.classList.contains('show-help-info')){
+      document.body.classList.remove('show-help-info');
+    } else {
+      document.body.classList.add('show-help-info');
+    }
   });
 }
 
