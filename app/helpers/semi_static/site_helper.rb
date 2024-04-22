ea90b674776b3780ce260856b337fe6ad2264dfe
brodjustice <brod@business-landing.com>
2024-04-22 14:25:28 +0200

    Fix bug in previous

--- app/helpers/semi_static/site_helper.rb
+++ app/helpers/semi_static/site_helper.rb
@@ -706,7 +706,7 @@ module SemiStatic
 
     def og_image_url
       if @entry&.get_page_attr('useAltImageAsMeta')
-        request.protocol + request.host + @entry.alt_img(:url)
+        request.protocol + request.host + @entry.alt_img(:original)
       elsif @entry&.img.present?
         # img_url_for_theme is actually always a relative path
         request.protocol + request.host + @entry.img_url_for_theme
